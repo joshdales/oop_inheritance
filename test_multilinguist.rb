1d9f215f9beece68e5d3ddd2ab711c8be10dc67e
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-11-09 10:05:38 -0500

    Set up test to work for language in

--- test_multilinguist.rb
+++ test_multilinguist.rb
@@ -3,5 +3,13 @@ require 'minitest/pride'
 require './multilinguist.rb'
 
 class TestMultilinguist < MiniTest::Test
-  
+
+  def setup
+    @multi = Multilinguist.new
+  end
+
+  def test_language_in
+    assert_equal "es", @multi.language_in("Spain")
+  end
+
 end
